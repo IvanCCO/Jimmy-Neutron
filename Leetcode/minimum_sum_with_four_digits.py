bbfef76844877bfb5d41961a702e6f11864a28b8
IvanCCO <ivanmedeiros0903@outlook.com>
2023-12-20 22:57:21 -0300

    Problem description minimum of two sum

---
+++ Leetcode/minimum_sum_with_four_digits.py
@@ -0,0 +1,15 @@
+# The input(k) is a integer number with four digits
+# and i have to split the number into two pieces, p1, p2
+# and return the minimum sum (p1 + p2)
+# Example:
+# k = 3023
+# The two pieces can be
+# A = p1=3 p2=023 where (p1 + p2) == 26  or
+# B = p1=30 p2=23 where (p1 + p2) == 53 or
+# C = p1=302 p2=3 where (p1 + p2) == 305
+# So the best solution is A and the return value is 26
+# Cause is the minimum value of the sum of the two parts
+
+def minimumSum(k : int):
+    return k
+
