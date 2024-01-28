0a1f1b66e1c5069029df9de2e043b0dc817fc24b
IvanCCO <ivanmedeiros0903@outlook.com>
2024-01-28 19:06:07 -0300

    palindrome

--- Leetcode/Main.java
+++ Leetcode/Main.java
@@ -1,53 +1,93 @@
+import java.nio.charset.CharsetEncoder;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Map;
 
 public class Main {
 
-  public static int majorityElement(int[] nums) {
+    public static int majorityElement(int[] nums) {
 
-    Map<Integer, Integer> hash = new HashMap<>();
+        Map<Integer, Integer> hash = new HashMap<>();
 
-      for (int num : nums) {
+        for (int num : nums) {
 
-          hash.put(num, (hash.getOrDefault(num, 0) + 1));
+            hash.put(num, (hash.getOrDefault(num, 0) + 1));
 
-          if (hash.get(num) > nums.length / 2) {
-              return num;
-          }
+            if (hash.get(num) > nums.length / 2) {
+                return num;
+            }
 
-      }
+        }
 
-    return 0;
+        return 0;
 
-  }
+    }
 
+    public static boolean isAPalindrome(String word) {
 
-  public static void rotate(int[] nums, int k){
+        char[] strArr = word.replaceAll("[^a-zA-Z0-9]", "").toLowerCase().toCharArray();
+        int lastPosition = strArr.length - 1;
 
-      for(int i = 0; i<k; i++){
+        for (int i = 0; i < strArr.length; i++) {
 
-          // trocar ultimo com primeiro
-          int aux = nums[0];
-          nums[0] = nums[nums.length-1];
+            if (strArr[i] != strArr[lastPosition - i]) {
+                return false;
+            }
 
-          for(int j = 1; j< nums.length; j++){
-
-             int aux2 = nums[j];
-
-             nums[j] = aux;
-
-             aux = aux2;
-          }
-      }
-  }
-
-  public static void main(String[] args) {
-
-    int[] num = { 1,2,3,4,5,6,7 };
+        }
+        return true;
+    }
 
 
-    rotate(num, 3);
+    public static void rotate(int[] nums, int k) {
 
-  }
+        for (int i = 0; i < k; i++) {
+
+            // trocar ultimo com primeiro
+            int aux = nums[0];
+            nums[0] = nums[nums.length - 1];
+
+            for (int j = 1; j < nums.length; j++) {
+
+                int aux2 = nums[j];
+
+                nums[j] = aux;
+
+                aux = aux2;
+            }
+        }
+    }
+
+    public static boolean wordPattern(String pattern, String s) {
+
+        String[] words = s.split(" ");
+        char[] arr = pattern.toCharArray();
+        int currentPositionPattern = 0;
+
+        Map<Character, String> map = new Hashtable<Character, String>();
+
+
+        for (Character letter : arr) {
+
+            if (!map.containsKey(letter)) {
+                map.put(letter, words[currentPositionPattern]);
+                currentPositionPattern++;
+            } else {
+                if (map.get(letter).equals(words[currentPositionPattern])) {
+                    currentPositionPattern++;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(wordPattern("abaa", "cat dog cat cat"));
+
+    }
 }
