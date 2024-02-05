22aca6c9ea35623a325b4685d01ac53da0e58583
IvanCCO <ivanmedeiros0903@outlook.com>
2024-02-05 20:03:02 -0300

    Length of last word

--- Leetcode/java/Main.java
+++ Leetcode/java/Main.java
@@ -257,6 +257,17 @@ public class Main {
         return x[x.length - 1].length();
     }
 
+    public int lengthOfLastWord2(String s) {
+        int count = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                count++;
+            } else if (count > 0) {
+                return count;
+            }
+        }
+        return count;
+    }
 
 
     public static void main(String[] args) {
