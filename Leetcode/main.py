cc8bccbafe2981cf071bb1c21fd13fd24d10efa3
IvanCCO <ivan.miranda@sptech.school>
2024-11-22 13:33:23 -0300

    check valid stack
    Consegui deixar com 0ms beats 100%

--- Leetcode/main.py
+++ Leetcode/main.py
@@ -96,5 +96,126 @@ class Solution:
 
         return self.quickSort(less) + [pivot] + self.quickSort(greater)
 
+    def checkValid(self, matrix: List[List[int]]) -> bool:
+        # iterar nas linhas e verificar se as linhas são validas
+        n = len(matrix)
+
+        for row in matrix:
+            if len(set(row)) != n:
+                return False
+
+        for colum in zip(*matrix):
+            if len(set(colum)) != n:
+                return False
+
+        return True
+
+    def longestConsecutive(self, nums: List[int]) -> int:
+
+        # Funciona mas não está otimizado
+        #
+        # o(n)
+        new_set = set(nums)
+        res = 0
+
+        # o(n) * o(n) = o(n**2)
+        for num in nums:
+            
+            search_number = num
+            consectiveCounter = 0
+
+            # o(n)
+            while search_number in new_set:
+                search_number += 1
+                consectiveCounter += 1
+
+            res = max(res, consectiveCounter)
+
+
+        return res
+
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+
+        res = []
+
+        nums.sort()
+
+        for fixed in range(len(nums)):
+
+            if fixed > 0 and nums[fixed] == nums[fixed - 1]:
+                continue
+
+            left = fixed + 1
+
+            right = len(nums) - 1
+
+
+            while left < right:
+
+                total = nums[fixed] + nums[left] + nums[right]
+
+                if total < 0:
+                    left += 1
+                elif total > 0:
+                    right -= 1
+                else:
+                    res.append([nums[fixed] , nums[left] , nums[right]])
+                    left += 1
+
+                    while nums[left] == nums[left - 1] and left < right:
+                        left += 1
+
+
+        return res
+
+    def maxArea(self, height: List[int]) -> int:
+
+        left = 0
+        right = len(height) - 1
+
+        max_area = 0
+
+        while left < right:
+
+            base = right - left
+            altura = min(height[right], height[left])
+
+            max_area = max(max_area, base * altura)
+
+            if height[left] <= height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return max_area
+
+    def isValid(self, s: str) -> bool:
+        
+        close_carecters = [")", "]", "}"]
+        expected_close = []
+
+
+        for char in s:
+            if char in close_carecters:
+                if len(expected_close) == 0:
+                    return False
+                
+                if not expected_close.pop() == char:
+                    return False
+            
+            if char == '(':
+                expected_close.append(')')
+            elif char == '[':
+                expected_close.append(']')
+            elif char == '{':
+                expected_close.append('}')
+
+        return len(expected_close) == 0
+
+
+
+
+
 s = Solution()
 
+print(s.isValid("()"))
